e133f7a9ec02b5d7a255e1b9a858002f9e31f1cd
mahdiajroush <mahdi.ajroush@gmail.com>
2024-08-12 11:37:55 +0300

    added author

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -22,6 +22,10 @@ const blogSchema = new Schema({
         type: String,
         required: true
     },
+    author: {
+        type: String,
+        required: true
+    },
     category: {
         type: String,
         required: true

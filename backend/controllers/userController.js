0f555546f3870f699ca0c340aa840081eed55c49
mahdiajroush <mahdi.ajroush@gmail.com>
2024-08-14 10:06:50 +0300

    all

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
   }
+  
 
 //register user
 const registeruser= async (req,res) => {
